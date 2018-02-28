e12c32bbe0d4714f7b8730b5b8c86fed79b9ef04
rakibul-bashar <rakibulbasharrakib@gmail.com>
2018-02-28 23:33:48 +0600

    deleted

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    mongoURI:'mongodb://rakib:rakib@ds229648.mlab.com:29648/storybooks',
-    googleClientID:'991546036537-6of19ba88a57mit0b0pbemat7qjg29o2.apps.googleusercontent.com',
-    googleClientSecret:'NKh12jokuN1cu55pmaaaGNMB'
-}
